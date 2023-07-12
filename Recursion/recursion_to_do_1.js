2af2122432e659007928c2115aa59486cd14ec92
Asad <abugosh94@gmail.com>
2023-07-12 16:37:50 +0300

    recursion to do 1

--- Recursion/recursion_to_do_1.js
+++ Recursion/recursion_to_do_1.js
@@ -23,3 +23,38 @@ function rFact(n){
 console.log(rFact(0));
 console.log(rFact(3));
 console.log(rFact(6.5));
+
+/*function floodFill(canvas2d, startXY, newColor){
+    startx = startXY[0];
+    starty = startXY[1];
+    console.log(startx);
+    startColor = canvas2d[starty][startx];
+    console.log(starty);
+    canvas2d[starty][startx] = newColor;
+    if(canvas2d[starty+1][startx] == startColor){
+        canvas2d[starty+1][startx]  = newColor;
+    }
+    else if(canvas2d[starty-1][startx] == startColor){
+        canvas2d[starty-1][startx]  = newColor;
+    }
+    else if(canvas2d[starty][startx+1] == startColor){
+        canvas2d[starty][startx+1]  = newColor;
+    }
+    else if(canvas2d[starty][startx+1] == startColor){
+        canvas2d[starty][startx+1]  = newColor;
+    }
+    else if(){
+        return canvas2d;
+    }
+
+    return floodFill(canvas2d, [ starty, startx], newColor);
+}
+
+canvas = [
+            [3,2,3,4,3],
+            [2,3,3,4,0],
+            [7,3,3,5,3],
+            [6,5,3,4,1],
+            [1,2,3,3,3]
+]
+console.log(floodFill(canvas, [2,2], 1));*/
